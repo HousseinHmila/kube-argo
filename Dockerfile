3553be78263b13c9e9f7afb4c2ca028000177d03
houssein <housseinhmila@gmail.com>
2024-02-16 15:13:19 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY ./py /app
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python3", "-m","flask","run","--host=0.0.0.0"]
---
+++ py/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <html>
+    <body>
+    <center>
+    <h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+    <br>
+    </center>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    print("Hello, World!")
+    app.run(debug=True)
